5ac6fc829bbe107e584f20e810cd7eee9e236e4a
dipika2589 <61450169+dipika2589@users.noreply.github.com>
2022-07-08 19:10:22 +0530

    Reorg in function

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,12 +42,19 @@ try:
 except URLError as e:
   streamlit.error()
   
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
+
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+
+#Snowflake-related functions
+def get_fruit_load_list:
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * from fruit_load_list")
+        return my_cur.fetchall()
+# Add button    
+if streamlit.button('Get Fruit load list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
 # Ask user to input fruit name
 add_fruit = streamlit.text_input('What fruit would you like to add?')
